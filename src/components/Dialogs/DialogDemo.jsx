053fa02a640eda062df71b9a57e813bcff040281
genarossi19 <genaros.gr@gmail.com>
2025-04-21 10:10:39 -0300

    correccion error de zoom en mobile

--- src/components/Dialogs/DialogDemo.jsx
+++ src/components/Dialogs/DialogDemo.jsx
@@ -20,7 +20,7 @@ export default function DialogDemo() {
     {
       title: "Aplicación con drones",
       description: "Descubre la última tecnología en aplicación",
-      image: "/dron2.jpeg",
+      image: "/dron2.webp",
       link: "/servicios/drones/",
     },
   ];
